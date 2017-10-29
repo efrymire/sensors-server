b5dea4fa742eec55c77bb5d285ddd8bee8bbade1
Ellie Frymire <fryme970@newschool.edu>
2017-10-29 20:59:29 +0000

    testing updates

--- add-sensor-data.js
+++ add-sensor-data.js
@@ -2,15 +2,17 @@ var request = require('request');
 const { Client } = require('pg');
 
 // PARTICLE PHOTON
-var device_id = process.env.PHOTON_ID;
+var device_id = '210023001247343432313031';
 var access_token = process.env.PHOTON_TOKEN;
 var particle_variable = 'json';
 var device_url = 'https://api.particle.io/v1/devices/' + device_id + '/' + particle_variable + '?access_token=' + access_token;
 
+// console.log(device_url)
+
 // AWS RDS POSTGRESQL INSTANCE
 var db_credentials = new Object();
 db_credentials.user = 'ellie';
-db_credentials.host = process.env.AWSRDS_EP;
+db_credentials.host = 'datastructures.cvnlstce0av9.us-east-2.rds.amazonaws.com';
 db_credentials.database = 'sensorData';
 db_credentials.password = process.env.AWSRDS_PW;
 db_credentials.port = 5432;
@@ -23,6 +25,10 @@ var getAndWriteData = function() {
         var ir = JSON.parse(device_json_string).ir;
         var tempC = JSON.parse(device_json_string).temp;
 
+        // console.log(device_json_string);
+        // console.log(ir);
+        // console.log(tempC);
+        
         // Connect to the AWS RDS Postgres database
         const client = new Client(db_credentials);
         client.connect();
@@ -40,4 +46,6 @@ var getAndWriteData = function() {
 };
 
 // write a new row of sensor data every five minutes
-setInterval(getAndWriteData, 300000);
+setInterval(getAndWriteData, 50000);
+
+// getAndWriteData()
