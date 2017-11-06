3bebf1b60bfb20b6cf8b8a302f5c10e0f91696c8
Ellie Frymire <fryme970@newschool.edu>
2017-11-06 16:38:55 +0000

    week 10

---
+++ expressJSON.js
@@ -0,0 +1,38 @@
+var express = require('express'),
+    app = express();
+const { Pool } = require('pg');
+
+// AWS RDS POSTGRESQL INSTANCE
+var db_credentials = new Object();
+db_credentials.user = 'aaron';
+db_credentials.host = process.env.AWSRDS_EP;
+db_credentials.database = 'sensors';
+db_credentials.password = process.env.AWSRDS_PW;
+db_credentials.port = 5432;
+
+app.get('/', function(req, res) {
+    // Connect to the AWS RDS Postgres database
+    const client = new Pool(db_credentials);
+
+    // SQL query
+    var q = `SELECT EXTRACT(HOUR FROM sensortime) as sensorhour,
+             EXTRACT(DAY FROM sensortime AT TIME ZONE 'America/New_York') as sensorday, 
+             EXTRACT(MONTH FROM sensortime AT TIME ZONE 'America/New_York') as sensormonth, 
+             count(*) as num_obs, 
+             sum(irsensor) as ir_count,
+             max(tempsensor) as max_temp, 
+             min(tempsensor) as min_temp
+             FROM sensordata 
+             GROUP BY sensormonth, sensorday, sensorhour;`;
+             
+    client.connect();
+    client.query(q, (qerr, qres) => {
+        res.send(qres.rows);
+        console.log('responded to request');
+    });
+    client.end();
+});
+
+app.listen(3000, function() {
+    console.log('Server listening...');
+});
